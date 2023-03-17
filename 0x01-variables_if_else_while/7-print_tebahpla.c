e7ad8e526da20323e5fee03f0271cb07d666bae9
el batouri <elbatouri@gmail.com>
2023-03-17 16:41:16 -0700

    last update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet reversed,
+ * and after that a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
