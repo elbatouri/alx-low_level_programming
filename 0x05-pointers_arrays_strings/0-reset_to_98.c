794a92908ede6352ed752672e233dac1e7336eb5
el batouri <elbatouri@gmail.com>
2023-03-27 18:39:08 -0700

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,9 @@
 /**
 * reset_to_98 - this function takes a pointer to an int as parameter
 * and after that updates the value it points to 98
-* input:@n
+* @n: input
 * Return:n
 */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
