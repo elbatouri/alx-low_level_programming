fe1212098ba5c36bdf826f90401b5d16a03b4855
el batouri <elbatouri@gmail.com>
2023-04-03 16:40:47 -0700

    update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: the string to locate the charcacter in
+ * @c: the character to locate
+ * @i: index to start
+ *
+ * Return: the first occurence of @c or null if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return ('\0');
+}
