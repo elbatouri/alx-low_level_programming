3d7ae020ea5ff3af68bfab444133af0faf42b9d3
el batouri <elbatouri@gmail.com>
2023-04-03 16:43:54 -0700

    fix error

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,18 +5,17 @@
  * _strchr -  locates a character in a string
  * @s: the string to locate the charcacter in
  * @c: the character to locate
- * @i: index to start
  *
  * Return: the first occurence of @c or null if not found
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int index;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (index = 0; s[index] >= '\0'; index++)
 	{
-		if (s[i] == c)
-			return (s + i);
+		if (s[index] == c)
+			return (s + index);
 	}
 	return ('\0');
 }
