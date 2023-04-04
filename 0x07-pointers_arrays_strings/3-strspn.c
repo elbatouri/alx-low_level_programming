769a50331102fc28be6d77dd3b5269b7e41bbd42
el batouri <elbatouri@gmail.com>
2023-04-03 17:53:29 -0700

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,6 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int a = 0;
 	int b;
-
 	while (*s)
 	{
 		for (b = 0; accept[b]; b++)
@@ -26,6 +25,7 @@ unsigned int _strspn(char *s, char *accept)
 			else if (accept[b + 1] == '\0')
 				return (a);
 		}
+		s++;
 	}
-	s++
+	return (a);
 }
