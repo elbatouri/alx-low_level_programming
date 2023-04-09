5f5076b6e7c39a8870b591ab38e893150843487f
el batouri <elbatouri@gmail.com>
2023-04-08 20:04:54 -0700

    create all object files and add them to library

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: the string to searched.
+ * @needle: the sustring
+ *
+ * Return: a pointer the sustring if matched and null if not.
+ */
+
+	char *_strstr(char *haystack, char *needle)
+	{
+		for (; *haystack != '\0'; haystack++)
+		{
+			char *a = haystack;
+			char *b = needle;
+
+			while (*a == *b && *b != '\0')
+			{
+				a++;
+				b++;
+			}
+			if (*b == '\0')
+				return (haystack);
+		}
+		return (0);
+	}
