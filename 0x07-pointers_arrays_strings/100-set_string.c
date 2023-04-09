f4c2fa658d60fbd10936ab6bcf64d21632c10cb2
el batouri <elbatouri@gmail.com>
2023-04-08 20:30:06 -0700

    changed

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -7,8 +7,7 @@
  * @to: the caracther that the value ll set to.
  *
  */
-
-	void set_string(char **s, char *to)
+void set_string(char **s, char *to)
 	{
 		*s = to;
 	}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0
+ *@
