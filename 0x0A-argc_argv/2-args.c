126221198cb999874eb139a3a1670b2dcd6d2014
el batouri <elbatouri@gmail.com>
2023-04-10 18:14:14 -0700

    updated

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print all arguments received.
+ *@argc: the number of arguments.
+ *@argv: the arguments array.
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
