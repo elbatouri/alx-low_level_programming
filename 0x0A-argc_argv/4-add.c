fb684fa3072881361261f6fbf666c037264bde34
el batouri <elbatouri@gmail.com>
2023-04-10 21:10:08 -0700

    updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,14 +10,6 @@
  * Return: 0
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
 int main(int argc, char *argv[])
 {
 	int sum = 0;
