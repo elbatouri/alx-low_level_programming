5956816205771726cdaf9dd1a71cdee882a71a9a
el batouri <elbatouri@gmail.com>
2023-04-10 17:58:58 -0700

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments.
+ * @argv: the array of arguments.
+ * @argc: the number of arguments.
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; /*ignore the argv*/
+	printf("%d\n", argc - 1);
+	
+	return (0);
+}
