5f2b334b8bea61d9b55b340dfd6cbfc3f1be22cf
el batouri <elbatouri@gmail.com>
2023-04-15 16:33:26 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,12 +12,13 @@
 
 	char *_memset(char *s, char b, unsigned int n)
 	{
-	char *p = s;
+	unsigned int i;
 
-	while (n--)
-		*s++ = b;
-
-	return (p);
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+	return (s);
 	}
 
 /**
