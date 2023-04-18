cc54721ac99a2db208683a1c8103d56d2b05fcdc
el batouri <elbatouri@gmail.com>
2023-04-17 20:23:30 -0700

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,27 +2,27 @@
 #define DOG_H
 
 
-   /**
-    * struct dog - dog clas
-    * @name: Name of the dog
-    * @age: Age of the dog
-    * @owner: Owner of the do
-    */
-
-
-   struct dog
-{
-   char *name;
-   float age;
-   char *owner;
-};
+	/**
+	 * struct dog - a dog's basic info
+	 * @name: First member
+	 * @age: Second member
+	 * @owner: Third member
+	 *
+	 * Description: Longer description
+	 */
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
 
 
 	/**
-	 *dog_t - typedef for struct dog
+	 * dog_t - typedef for struct dog
 	 */
-
 	typedef struct dog dog_t;
+
 
 	void init_dog(struct dog *d, char *name, float age, char *owner);
 	void print_dog(struct dog *d);
@@ -32,6 +32,4 @@
 	int _strlen(char *s);
 
 
-
-#endif
-
+	#endif
