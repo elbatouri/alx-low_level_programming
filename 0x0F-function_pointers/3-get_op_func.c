748b4178f131c70993800fc61962f00fce8051c1
el batouri <elbatouri@gmail.com>
2023-04-19 19:39:58 -0700

    update everything at once

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -12,7 +12,7 @@
 	int (*get_op_func(char *s))(int, int)
 	{
 		op_t ops[] = {
-			
+
 			{"+", op_add},
 			{"-", op_sub},
 			{"*", op_mul},
@@ -21,7 +21,7 @@
 			{NULL, NULL}
 		};
 	int i = 0;
-	
+
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
 	return (ops[i].f);
