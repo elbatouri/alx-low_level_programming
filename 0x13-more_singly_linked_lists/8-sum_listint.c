ec0718cbbd0dbb4c714a8c0128e02f7a3c15884a
elbatouri <elbatouri@gmail.com>
2023-05-02 19:05:36 -0700

    update

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - the sum of all the data (n) of a linked lists
+ * @head : the fist node of the linked list
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *temporary = head;
+	int sum = 0;
+
+	while (temporary)
+	{
+		sum += temporary->n;
+		temporary = temporary->next;
+	}
+	return (sum);
+}
