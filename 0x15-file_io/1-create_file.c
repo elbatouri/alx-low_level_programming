b4b0cef6fd4c2be593608f1fb5765e81be208978
elbatouri <elbatouri@gmail.com>
2023-05-09 14:53:41 -0700

    update the task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,10 +19,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-
-	if (text_content == NULL)
-		return (-1);
-
 	for (length = 0; text_content[length];)
 		length++;
 
