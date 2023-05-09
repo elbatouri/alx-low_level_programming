b608b4b22d707c475289154c64817e4c1772c7f0
elbatouri <elbatouri@gmail.com>
2023-05-09 15:04:17 -0700

    update 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 
 	{
@@ -25,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		length++;
 	}
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename,  O_CREAT | O_RDWR | O_TRUNC, 0600);
 	Written = write(fd, text_content, length);
 
 	if (fd < 0)
