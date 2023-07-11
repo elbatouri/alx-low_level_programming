20688c451c8146fd09cbb9163fc9173d98b3765c
el batouri rabi <elbatouri@gmail.com>
2023-07-10 17:19:37 -0700

    correct the 1-main.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
-	
+
 		length = strlen(text_content);
 
 	fd = open(filename,  O_CREAT | O_RDWR | O_TRUNC, 0600);
