794252de51f6be6348385f26f889342df36e7271
el batouri rabi <elbatouri@gmail.com>
2023-09-08 13:16:56 -0700

    first task push

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates hash table.
+ * @size: the array's size
+ *
+ * Return: error case - NULL
+ * 			other - pointer to new hash table.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *sht;
+	unsigned long int i;
+
+	sht = malloc(sizeof(hash_table_t));
+	if (sht == NULL)
+		return (NULL);
+
+	sht->size = size;
+	sht->array = malloc(sizeof(hash_node_t*) * size);
+	if (sht->array == NULL)
+		return (NULL);
+	for (i = 0; i <size; i++)
+		sht->array[i] = NULL;
+	return(sht);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+this is README file of the project 0x1A-hash_tables
