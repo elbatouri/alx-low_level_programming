a8502e76cd53c5437ae719b39cdd5c26b344ca57
elbatouri rabi <elbatouri@gmail.com>
2023-12-12 07:37:38 -0800

    fix betty check on task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,13 +12,13 @@ int linear_search(int *array, size_t size, int value)
 	size_t i = 0;
 
 	if (array == NULL)
-		return(-1);
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 
-		printf("Value checked array[%d] = [%d]\n", (int) i, (int) array[i]);
-	       if (array[i] == value)
-		       return (i);
+		printf("Value checked array[%d] = [%d]\n", (int) i, (int) array[i])
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
