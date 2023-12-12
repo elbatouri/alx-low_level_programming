13b64bc2c974cee171fa918280c5c56d244a207c
elbatouri rabi <elbatouri@gmail.com>
2023-12-12 07:32:22 -0800

    linear search push

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linears earches for a value in an array
+ * @array: the numbers set
+ * @size: siet of the array
+ * @value: searched value
+ * Return: return the matched index
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return(-1);
+	for (i = 0; i < size; i++)
+	{
+
+		printf("Value checked array[%d] = [%d]\n", (int) i, (int) array[i]);
+	       if (array[i] == value)
+		       return (i);
+	}
+	return (-1);
+}
